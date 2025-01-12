ceb845f24d053abf4c9d02146c8ee9008e98fbb3
Ivann Comiso <ivannc@Ivanns-MacBook-Pro.local>
2025-01-13 02:41:29 +0800

    first commit

--- vite.config.ts
+++ vite.config.ts
@@ -1,15 +1,15 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [react()],
-  base: "gorf",
+  base: "/gorff/", // Set the base URL to match your GitHub Pages URL
   server: {
     port: 5173,
-    open: true
+    open: true,
   },
   build: {
     outDir: 'dist',
-    sourcemap: true
-  }
-})
+    sourcemap: true,
+  },
+});
